7bf64d9227f6519561bb8c2999099e83a1c0090e
Saqib Naseer <cyborgztech369@gmail.com>
2026-07-06 01:03:42 +0530

    Delete 01_Python_Basics/example1/hello_world.py

--- 01_Python_Basics/example1/hello_world.py
+++
@@ -1,6 +0,0 @@
-print("Hello World")
-
-"""output
-Hello World
-it means when you put print("Hello World") in any IDE ( integrated developement Environment) like vs code you see the output
-Hello World"""
