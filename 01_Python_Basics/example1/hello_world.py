b9b359a8caed6f16bb981c649e6c8307e8448962
Saqib Naseer <cyborgztech369@gmail.com>
2026-07-06 00:54:57 +0530

    Update hello_world.py

--- 01_Python_Basics/example1/hello_world.py
+++ 01_Python_Basics/example1/hello_world.py
@@ -1,1 +1,6 @@
+print("Hello World")
 
+"""output
+Hello World
+it means when you put print("Hello World") in any IDE ( integrated developement Environment) like vs code you see the output
+Hello World"""
